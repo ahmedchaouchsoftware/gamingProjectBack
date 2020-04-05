ac179c34b7680ae180c0846137e3fd2ec73a212f
ahmedchaouchsoftware <ing.chaouch.ahmed@gmail.com>
2020-04-05 19:25:53 +0200

    initial commit

---
+++ src/main/java/fr/entertainment/gaming/GamingApplication.java
@@ -0,0 +1,13 @@
+package fr.entertainment.gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingApplication.class, args);
+	}
+
+}
